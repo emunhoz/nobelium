99b93f7b3c4bb6c38f4797fac27a774384b47c77
emunhoz <edermunhozsantos@gmail.com>
2022-06-01 21:57:55 -0300

    chore: add gittalk

--- blog.config.js
+++ blog.config.js
@@ -42,13 +42,13 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: 'gitalk', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
-      repo: '', // The repository of store comments
-      owner: '',
-      admin: [],
-      clientID: '',
-      clientSecret: '',
+      repo: 'nobelium', // The repository of store comments
+      owner: 'emunhoz',
+      admin: ['emunhoz'],
+      clientID: process.env.GIT_TALK_CLIENT_ID,
+      clientSecret: process.env.GIT_TALK_CLIENT_SECRET,
       distractionFreeMode: false
     },
     utterancesConfig: {
